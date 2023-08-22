6f4e97fdf241f444711db40061bcfc7d4073c397
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-22 10:38:41 -0500

    keys are correct

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,22 +2,22 @@
 
 --Insert "names" departments into table--
 INSERT INTO department (id, name) 
-VALUES (1, "IT"), 
-(2, "Finance"), 
-(3, "Marketing"), 
-(4, "HR");
+VALUES (001, "IT"), 
+(002, "Finance"), 
+(003, "Marketing"), 
+(004, "HR");
 
 --Insert "roles" into the table--
 INSERT INTO role(id, title, salary, department_id ) 
-VALUES (1, "Full-Stack-Developer", 105324.98, 1),
-(2, "Accountant", 95000, 2),
-(3, "Sales Manager", 76000, 3),
-(4, "Human Resources Specialist", 65000, 4);
+VALUES (001, "Full-Stack-Developer", 105324.98, 001),
+(002, "Accountant", 95000, 002),
+(003, "Sales Manager", 76000, 003),
+(004, "Human Resources Specialist", 65000, 004);
 
 -- Insert employees to the database--
 --CAN YOU INSERT MANAGER?
 INSERT INTO employee(id, first_name, last_name , , role_id, department_id)
-VALUES (1, "Samantha", "Gonzalez", 1),
-(2, "Valerie", "Espinoza" 2),
-(3, "Jennifer", "Brown", 3),
-(4, "Jordan", "Skrewzek", 4);
+VALUES (001, "Samantha", "Gonzalez", 001),
+(002, "Valerie", "Espinoza" 002),
+(003, "Jennifer", "Brown", 003),
+(004, "Jordan", "Skrewzek", 004);
