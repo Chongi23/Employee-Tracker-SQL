52e3729e924a2282ea364c99d27a3e84129d83c9
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-22 11:23:17 -0500

    fixed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,8 +15,8 @@ VALUES (001, "Full-Stack-Developer", 105324.98, 001),
 (004, "Human Resources Specialist", 65000, 004);
 
 
-INSERT INTO employee (id, first_name, last_name, role_id, department_id)
-VALUES (001, 'Samantha', 'Gonzalez', 001, 001),
-(002, 'Valerie', 'Espinoza', 002, 002),
-(003, 'Jennifer', 'Brown', 003, 003),
-(004, 'Jordan',  'Skrewzek', 004, 004);
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (001, 'Samantha', 'Gonzalez', 001),
+(002, 'Valerie', 'Espinoza', 002),
+(003, 'Jennifer', 'Brown', 003),
+(004, 'Jordan',  'Skrewzek', 004);
