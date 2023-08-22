68f26488ccc8be167a384dddc6c066a7d11d42c5
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-08-22 09:19:43 -0500

    seeded data

--- db/schema.sql
+++ db/schema.sql
@@ -11,20 +11,24 @@ CREATE TABLE department (
 
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id NOT NULL INT,
     title VARCHAR(30),
-    salary DECIMAL 
-    
+    salary DECIMAL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (department_id),
+    REFERENCES department(id) ON DELETE CASCADE
 );
 
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL,
     first_name VARCHAR(30)NOT NULL,
     last_name VARCHAR(30)NOT NULL,
     role_id INT,
-    manager_id INT
-    
+    department_id INT,
+    PRIMARY KEY(id)
+    FOREIGN KEY (department_id, role_id),
+    REFERENCES department(id), role(id)
     
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,23 @@
+
+
 --Insert "names" departments into table--
-INSERT INTO department (name) 
-VALUES ("IT"), 
-("Finance"), 
-("Marketing"), 
-("HR");
+INSERT INTO department (id, name) 
+VALUES (1, "IT"), 
+(2, "Finance"), 
+(3, "Marketing"), 
+(4, "HR");
 
 --Insert "roles" into the table--
-INSERT INTO role(title, salary, department_id ) 
-VALUES ('Full-Stack-Developer', 105324.98, 1),
-('Accountant', 95000, 2),
-('Sales Manager', 76000, 3),
-('Human Resources Specialist', 65000, 4);
+INSERT INTO role(id, title, salary, department_id ) 
+VALUES (1, "Full-Stack-Developer", 105324.98, 1),
+(2, "Accountant", 95000, 2),
+(3, "Sales Manager", 76000, 3),
+(4, "Human Resources Specialist", 65000, 4);
 
 -- Insert employees to the database--
-INSERT INTO employee(first_name, last_name ,role_id, manager_id)
-VALUES ('Samantha', 'Gonzalez', 1, 2),
-('Valerie', 'Espinoza' 2, 3),
-('Jennifer', 'Brown', 3, 1),
-('Jordan', 'Skrewzek' 4, 4);
+--CAN YOU INSERT MANAGER?
+INSERT INTO employee(id, first_name, last_name , , role_id, department_id)
+VALUES (1, "Samantha", "Gonzalez", 1),
+(2, "Valerie", "Espinoza" 2),
+(3, "Jennifer", "Brown", 3),
+(4, "Jordan", "Skrewzek", 4);
